ea3d6b36597257cca8cbfb6746f2ce953e4fcbfe
Oyunaa <oyuka579@gmail.com>
2023-05-01 10:55:58 +0800

    update

--- src/pages/components/Meta.tsx
+++ src/pages/components/Meta.tsx
@@ -14,11 +14,12 @@ const Meta = ({ metaData }: { metaData: IMeta }) => {
     <Head>
       <title>Web site</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="title" content={metaData?.title} />
-      <meta name="description" content={metaData?.description} />
-
+      <meta name="description" content={"Test NEXTJS desc"} />
       <meta name="site_name" content={metaData?.site_name} />
-      <meta name="image" content={metaData?.image} />
+
+      <meta name="og:title" content={metaData?.title} />
+      <meta name="og:description" content={metaData?.description} />
+      <meta name="og:image" content={metaData?.image} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
